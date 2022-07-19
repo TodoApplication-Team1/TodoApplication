b7f96cdd222ffa1a09d62bc4398505900abd1c9b
Divya <Divya.Peter@skillassure.com>
2022-07-19 17:03:08 +0530

    task updated

--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'app-task',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TaskComponent implements OnInit {
 
-  constructor() { }
+  constructor(private modalService: NgbModal) { }
 
   ngOnInit(): void {
   }
+  
+  
+  
+  // let datetimepicker: DateTimePicker = new DateTimePicker({
+  //   renderDayCell: disableDate,
+  //   placeholder: 'Select a date and time',
+  //   width: "233px"
+  // });
+  // datetimepicker.appendTo('#datetimepicker');
+  // datetimepicker.show();
 
+  // function disableDate(args: RenderDayCellEventArgs): void {
+  //   /*Date need to be disabled*/
+  //   if (args.date.getDay() === 0 || args.date.getDay() === 6) {
+  //       args.element.classList.add('e-highlightweekend');
+  //   }
+  // }
 }
+  // 
+
